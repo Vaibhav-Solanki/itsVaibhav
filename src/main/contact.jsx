3c018127a9e5a73adc86e7d241543df3db2bc3ea
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-03-22 04:48:34 +0530

    contact

--- src/main/contact.jsx
+++ src/main/contact.jsx
@@ -7,31 +7,73 @@ function Contact() {
       </h2>
       <div className="col" id="colP">
         <div class="socialGrid">
-          <div className="socDiv">
+          <div
+            className="socDiv"
+            onClick={() =>
+              window.open(
+                "https://www.linkedin.com/in/vaibhav-s-solanki/",
+                "_blank"
+              )
+            }
+          >
             <i class="fa-brands fa-linkedin-in"></i>
           </div>
-          <div className="socDiv">
+          <div
+            className="socDiv"
+            onClick={() =>
+              window.open("https://github.com/Vaibhav-Solanki", "_blank")
+            }
+          >
             <i class="fa-brands fa-github"></i>
           </div>
-          <div className="socDiv">
+          <div
+            className="socDiv"
+            onClick={() =>
+              window.open("mailto:vaibhavsolankie@gmail.com", "_blank")
+            }
+          >
             <i class="fa-solid fa-envelope"></i>
           </div>
-          <div className="socDiv">
+          <div
+            className="socDiv"
+            onClick={() =>
+              window.open("https://www.instagram.com/inco.solanki/", "_blank")
+            }
+          >
             <i class="fa-brands fa-instagram"></i>
           </div>
-          <div className="socDiv">
+          <div
+            className="socDiv"
+            onClick={() =>
+              window.open("https://twitter.com/VaibhavSolankies", "_blank")
+            }
+          >
             <i class="fa-brands fa-twitter"></i>
           </div>
-          <div className="socDiv">
+          <div
+            className="socDiv"
+            onClick={() =>
+              window.open("https://www.hackerrank.com/vaibhavsolanki", "_blank")
+            }
+          >
             <i class="fa-brands fa-hackerrank"></i>
           </div>
-          <div className="socDiv">
+          <div
+            className="socDiv"
+            onClick={() => window.open("https://dev.to/inco", "_blank")}
+          >
             <i class="fa-brands fa-dev"></i>
           </div>
-          <div className="socDiv">
+          <div
+            className="socDiv"
+            onClick={() => window.open("tel:+917000392596", "_blank")}
+          >
             <i class="fa-solid fa-phone"></i>
           </div>
-          <div className="socDiv">
+          <div
+            className="socDiv"
+            onClick={() => window.open("https://wa.me/7000392596", "_blank")}
+          >
             <i class="fa-brands fa-whatsapp"></i>
           </div>
         </div>
