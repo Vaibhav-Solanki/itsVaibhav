36bab2be821219babff4d4b0868c8fe02c78bac2
Vaibhav Solanki <Vaibhavdasss@gmail.com>
2022-04-13 17:16:33 +0530

    yarn update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./anim.css";
-// import "./App.css";
+import "./App.css";
 import Header from "./comp/Header";
 import Footer from "./comp/Footer";
 import Main from "./comp/main";
